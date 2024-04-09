341504747b980b67551bcb8d6df8ec7cfa9d2029
Rick Astley <45075899+rickastlee@users.noreply.github.com>
2024-04-09 04:42:26 +0200

    Automatically launch MotoDialer after signing

--- signing/pull_sign_install.sh
+++ signing/pull_sign_install.sh
@@ -4,6 +4,8 @@ workdir="$HOME/MotoDialer/signing"
 src="/sdcard/MotoDialer/app/src/main/"
 filename="MotoDialer.apk"
 keystore="$workdir/lineage_apps.jks"
+packagename="com.android.dialer"
+mainactivity="$packagename.main.impl.MainActivity"
 
 cd $workdir
 adb wait-for-device
@@ -11,3 +13,4 @@ adb pull $src$filename
 apksigner sign --ks=$keystore $filename
 adb wait-for-device
 adb install $workdir/$filename
+adb shell am start -n $packagename/$mainactivity
