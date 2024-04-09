24c95e2c2e8ba3ae9d7302edac66b76e3cccf5c2
Rick Astley <45075899+rickastlee@users.noreply.github.com>
2024-04-09 04:55:46 +0200

    pull_sign_install.sh: Code cleanup
     - Only wait for establishing adb connection at the beginning
     - Use camelCase for variable names
     - Do not terminate src with /

--- signing/pull_sign_install.sh
+++ signing/pull_sign_install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
  
 workdir="$HOME/MotoDialer/signing"
-src="/sdcard/MotoDialer/app/src/main/"
-filename="MotoDialer.apk"
-keystore="$workdir/lineage_apps.jks"
-packagename="com.android.dialer"
-mainactivity="$packagename.main.impl.MainActivity"
+src="/sdcard/MotoDialer/app/src/main"
+fileName="MotoDialer.apk"
+keyStore="$workdir/lineage_apps.jks"
+packageName="com.android.dialer"
+mainActivity="$packageName.main.impl.MainActivity"
 
 cd $workdir
 adb wait-for-device
-adb pull $src$filename
-apksigner sign --ks=$keystore $filename
-adb wait-for-device
-adb install $workdir/$filename
-adb shell am start -n $packagename/$mainactivity
+adb pull $src/$fileName
+apksigner sign --ks=$keyStore $fileName
+adb install $workdir/$fileName
+adb shell am start -n $packageName/$mainActivity
